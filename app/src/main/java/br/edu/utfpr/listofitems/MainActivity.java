10440e98a3e16d429504a099837ef0cdb6454bcb
Ademir <ademir.m.junior@gmail.com>
2021-08-03 14:16:11 -0300

    Populate list

--- app/src/main/java/br/edu/utfpr/listofitems/MainActivity.java
+++ app/src/main/java/br/edu/utfpr/listofitems/MainActivity.java
@@ -3,12 +3,43 @@ package br.edu.utfpr.listofitems;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listViewItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listViewItems = findViewById(R.id.listViewItems);
+
+        populateListViewItems();
+    }
+
+    private void populateListViewItems() {
+        ArrayList<String> items = new ArrayList<>();
+
+        items.add(getString(R.string.rice));
+        items.add(getString(R.string.coffee));
+        items.add(getString(R.string.milk));
+        items.add(getString(R.string.chocolate));
+        items.add(getString(R.string.ketchup));
+        items.add(getString(R.string.cereal));
+        items.add(getString(R.string.apple));
+        items.add(getString(R.string.banana));
+        items.add(getString(R.string.bread));
+
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<>(this,
+                        android.R.layout.simple_list_item_1,
+                        items);
+
+        listViewItems.setAdapter(adapter);
     }
 }
